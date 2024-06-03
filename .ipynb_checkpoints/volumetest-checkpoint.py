f0abeb5eba0572d05c323f7cc8a1bbbaa518fad6
davidmertenjonestccs <davidm@claremont.edu>
2024-06-03 10:02:33 -0700

    credentials directory!

--- .ipynb_checkpoints/volumetest-checkpoint.py
+++ .ipynb_checkpoints/volumetest-checkpoint.py
@@ -6,7 +6,7 @@ import os
 
 os.system('ls')
 
-with open('config.yaml','r') as file:
+with open('/credentials/config.yaml','r') as file:
     config = yaml.safe_load(file)
     
 print(config['username'])
