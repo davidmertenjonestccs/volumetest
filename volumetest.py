fc78ff968f0d9d07ccab1671606684ba1a1a05b2
davidmertenjonestccs <davidm@claremont.edu>
2024-06-03 09:55:59 -0700

    yaml not yml?

--- volumetest.py
+++ volumetest.py
@@ -6,7 +6,7 @@ import os
 
 os.system('ls')
 
-with open('config.yml','r') as file:
+with open('config.yaml','r') as file:
     config = yaml.safe_load(file)
     
 print(config['username'])
@@ -16,6 +16,6 @@ config['timestamp'] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
 print(config['timestamp'])
 
-with open('config.yml', 'w') as file:
+with open('config.yaml', 'w') as file:
         yaml.dump(config, file, default_flow_style=False)
         
